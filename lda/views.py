ff6a3193063d9da4811d62b9bfff2ecca91742ec
maminnas <moin.aminnaseri@gmail.com>
2017-07-17 04:45:07 -0700

    fixed slow data passing

--- lda/views.py
+++ lda/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
@@ -30,7 +31,7 @@ def clean_words(topic, n):
 
 def index(request):
 	topic = topics.objects.all()
-	topic = clean_words(topic, 10)
+	topic = clean_words(topic, 8)
 	ser_topic = serializers.serialize('json', topic)
 	context = { 'topics': ser_topic }
 	return render(request, 'graph/graph.html', context)
@@ -40,8 +41,19 @@ def topic_over_time(request):
 	print ("Retrieving objects")
 	t1=time.time()
 	# data = doc_topic_distrib.objects.exclude(attributes = ["doc_id","title"]).values(datetime__year, datetime__month, datetime__day).aggregate(Max())
-	data = doc_topic_distrib.objects.all()
-	data = data[::100]
+	# data = doc_topic_distrib.objects.all()
+	# data = data[::100]
+	data=[]
+	i = 0
+	chunk=50
+	while True:#i<n:
+		try:
+			data += [doc_topic_distrib.objects.get(pk=i*chunk+1)]
+		except doc_topic_distrib.DoesNotExist:
+			# print ("Does not exist pk:", i*chunk+1)
+			break
+		i += 1
+
 	t2=time.time()
 	print ("objects retrieved in:", t2-t1)
 	ser = serializers.serialize('json', list(data))
